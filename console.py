8e0f60ba887d35667e8009d302cb1fcab1a0bb04
BLACKMouha <mohamedsenghor05gmail.com>
2023-03-12 12:01:15 +0000

    Console v1.0 | pycodestyle line 21

--- console.py
+++ console.py
@@ -18,7 +18,10 @@ class HBNBCommand(cmd.Cmd):
         Leaves the command interpreter
         '''
         return True
+
     def do_EOF(self, arg):
         return True
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
