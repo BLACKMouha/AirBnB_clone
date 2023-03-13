5871c09da368ca3bfa819a40550dcc265be46301
BLACKMouha <mohamedsenghor05gmail.com>
2023-03-13 10:38:09 +0000

    Handling particular command syntaxes

--- console.py
+++ console.py
@@ -164,7 +164,7 @@ class HBNBCommand(cmd.Cmd):
             print("** attribute name missing **")
             return
 
-        att = args[2]
+        att = args[2].replace("'", '').replace('"', '')
         if len(args) == 3:
             print("** value missing **")
             return
@@ -172,22 +172,19 @@ class HBNBCommand(cmd.Cmd):
             return
 
         val = args[3]
-        try:
-            if val[0] == '"' and val[-1] == '"':
-                val = str(val[1:-1])
-            elif '.' not in val:
-                val = int(val)
-            else:
-                val = float(val)
-        except Exception:
-            return
+        if val.isdigit():
+            val = int(val)
+        elif isfloat(val):
+            val = float(val)
+        else:
+            val = val.replace("'", '').replace('"', '')
 
         setattr(obj, att, val)
         obj.save()
 
     def do_count(self, line):
         '''
-        count commands to count the number of instances of a class
+        count command to count the number of instances of a class
         '''
         args = line.split()
         cls_name = None if len(args) == 0 else args[0]
@@ -207,56 +204,140 @@ class HBNBCommand(cmd.Cmd):
 
     def default(self, line):
         '''
-        Handles unrecognized commands syntax
+        Handles unrecognized syntax
+        Examples:
+            `ClassName.all() | ClassName.count()`
+            `ClassName.show(<id>) | ClassName.destroy(<id>)`
+            `ClassName.update(<id>, "<attribute>", "<value>")`
+            `ClassName.update(<id>, {"<att1>": "<val1>", "<att2>: <val2>})`
         '''
         cmd_funcs = {
-                'all': self.do_all,
-                'count': self.do_count
-                }
+            "all": self.do_all,
+            "count": self.do_count,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "update": self.do_update
+        }
 
         cleaned_line = rmtrchr(line)
+
         if '.' not in line:
-            print("*** Unknown syntax:", line)
+            print('*** Unknown syntax:', line)
             return
 
-        dot_idx = cleaned_line.find('.')  # Finding the first dot
-        if dot_idx == - 1:
-            print("*** Unknown syntax:", line)
+        dot_idx = cleaned_line.find('.')
+        if dot_idx == -1:
+            print('*** Unknown syntax:', line)
             return
+
         if dot_idx + 1 >= len(line):
-            print("*** Unknown syntax:", line)
+            print('*** Unknown syntax:', line)
             return
 
         cmd_cls = cleaned_line[:dot_idx]
-        if cmd_cls not in HBNBCommand.__classes:
-            print("*** Unknown syntax:", line)
+        if cmd_cls not in HBNBCommand.__classes.keys():
+            print('*** Unknown syntax:', line)
             return
 
         remain = cleaned_line[dot_idx + 1:]
 
         if '(' not in remain or ')' not in remain\
                 or ('(' in remain and remain[len(remain) - 1] != ')'):
-            print("*** Unknown syntax:", line)
+            print('*** Unknown syntax:', line)
             return
 
-        op_idx = remain.find('(')  # Finding the opening parenthesis
+        op_idx = remain.index('(')
         cmd_func = remain[:op_idx]
         if cmd_func not in cmd_funcs:
-            print("*** Unknown syntax:", line)
+            print('*** Unknown syntax:', line)
             return
 
         if remain.find('(', op_idx + 1) != -1:
-            print("*** Unknown syntax:", line)
+            print('*** Unknown syntax:', line)
             return
-
         if cmd_func in ['all', 'count']:
             cp_idx = remain.find(')', op_idx + 1)
-            if cp_idx != - 1 and remain.find(')', cp_idx + 1) != -1:
-                print("*** Unknown syntax:", line)
+            if cp_idx != -1 and remain.find(')', cp_idx + 1) != -1:
+                print('*** Unknown syntax:', line)
                 return
-            # At this stage, the line is perfected
+
             cmd_funcs[cmd_func](cmd_cls)
             return
+
+        if remain[op_idx + 1] not in ['"', "'"]:
+            print('*** Unknown syntax:', line)
+            return
+
+        if cmd_func in ['show', 'destroy']:
+            if (remain[op_idx + 1] == '"' and remain[len(remain) - 2] != '"')\
+                    or (remain[op_idx + 1] == "'"
+                        and remain[len(remain) - 2] != "'"):
+                print('*** Unknown syntax:', line)
+                return
+            cmd_arg = remain[op_idx + 2:len(remain) - 1]
+            cmd_arg = cmd_arg.replace('"', '').replace("'", '')
+
+            cmd_funcs[cmd_func](cmd_cls + ' ' + cmd_arg)
+            return
+
+        if cmd_func == 'update':
+            if '{' not in remain:
+                if ',' in remain and occurrence(remain, ',') == 2:
+                    the_args = remain[len(cmd_func) + 1:].split(',')
+                    if len(the_args) == 3:
+                        id_arg = the_args[0].replace('"', '').replace("'", '')
+                        key_arg = the_args[1].replace('"', '').replace("'", '')
+                        value_arg = the_args[2].replace(')', '')
+                        value_arg = value_arg.replace('"', '').replace("'", '')
+                        args = id_arg + ' ' + key_arg + ' ' + value_arg
+
+                        cmd_funcs[cmd_func](cmd_cls + ' ' + args)
+
+                    else:
+                        print('*** Unknown syntax:', line)
+                        return
+
+                else:
+                    print('*** Unknown syntax:', line)
+                    return
+
+            else:
+                if remain[len(remain) - 2] == '}':
+                    comma_idx = remain.find(',')
+                    if comma_idx == - 1:
+                        print('*** Unknown syntax:', line)
+                        return
+
+                    id_arg = remain[op_idx + 1:comma_idx].replace("'", '')
+                    id_arg = id_arg.replace('"', '')
+                    ob_idx = remain.find('{')
+                    cb_idx = remain.find('}')
+                    the_dict = remain[ob_idx + 1: cb_idx]
+
+                    if ',' not in the_dict:
+                        the_args = the_dict.split(':')
+                        if len(the_args) == 2:
+                            for i in range(len(the_args)):
+                                the_args[i].replace("'", '').replace('"', '')
+
+                            args = ' '.join([id_arg, the_args[0], the_args[1]])
+                            cmd_funcs[cmd_func](cmd_cls + ' ' + args)
+                        else:
+                            print('*** Unknown + syntax:', line)
+                            return
+                    else:
+                        key = value = args = ''
+                        kvs = the_dict.split(',')
+                        for i in kvs:
+                            kvp = i.split(':')
+                            if len(kvp) == 2:
+                                key = kvp[0]
+                                value = kvp[1]
+                                args = ' '.join([id_arg, key, value])
+                                cmd_funcs[cmd_func](cmd_cls + ' ' + args)
+                else:
+                    print('*** Unknown syntax:', line)
+                    return
 
     def do_clear(self, line):
         '''
@@ -266,15 +347,38 @@ class HBNBCommand(cmd.Cmd):
         system('clear')
 
 
+def occurrence(word, c):
+    '''Return the number of occurrence of a character in a string'''
+    count = 0
+    for i in word:
+        if i == c:
+            count += 1
+    return count
+
+
 def rmtrchr(line):
     '''
     Removes Trailing characters of a line
     '''
     chars = [chr(32), chr(8), chr(9), chr(10), chr(11), chr(12), chr(13)]
-    clean_line = ''
+    clea_line = ''
     for char in chars:
         clean_line = line.replace(char, '')
     return clean_line
+
+
+def isfloat(string):
+    '''Checks if a string can be converted to float
+        Return: True if the string can be converted into Python Float
+                False, otherwhise'''
+    if '.' in string and occurrence(string, '.') == 1:
+        n = string.replace('.', '')
+        if n.isdigit():
+            return True
+        else:
+            False
+    else:
+        False
 
 
 if __name__ == '__main__':
